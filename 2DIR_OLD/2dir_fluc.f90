60df5794705fc4d5a71bb8571a1ee44f55cbef51
Zeke Piskulich <piskulichz@gmail.com>
2023-10-10 16:59:44 -0400

    first commit

---
+++ 2DIR_OLD/2dir_fluc.f90
@@ -0,0 +1,140 @@
+!    Program to calculate the 2D-IR vibrational spectra
+!
+  Program spectra
+
+  use map_data
+  use time_data
+  use freq_data
+  use fluc_data
+  use constants
+
+  implicit none
+  integer :: ioh, iTw
+  double precision :: tstart, tend, read_time, tcf_time, ta, tb
+  double precision, allocatable, dimension(:) :: w01, w12, mu01, mu12
+  double precision, allocatable, dimension(:,:) :: eOH , dH
+  double complex, allocatable, dimension(:,:) :: tcf_rp, tcf_np, tcf_rp_tot, tcf_np_tot
+  double complex, allocatable, dimension(:,:,:) :: tcfH_rp, tcfH_np, tcfH_rp_tot, tcfH_np_tot
+
+  ! Read in the parameters for the calculation from spectra.in
+  call read_input
+  write(6,*) ' Read in input parameters'
+
+  ! Allocate arrays for calculation of the spectra
+  allocate(w01(ntimes)); allocate(mu01(ntimes)); allocate(w12(ntimes)); allocate(mu12(ntimes)); allocate(eOH(ntimes,3))
+  allocate(tcf_rp(0:ncorr,0:ncorr)); allocate(tcf_rp_tot(0:ncorr,0:ncorr))
+  allocate(tcf_np(0:ncorr,0:ncorr)); allocate(tcf_np_tot(0:ncorr,0:ncorr))
+
+  ! Allocated arrays for calculation of the spectra derivative
+  if(flag_fluc) then
+     allocate(dH(ntimes,5))
+     allocate(tcfH_rp(0:ncorr,0:ncorr,5)); allocate(tcfH_rp_tot(0:ncorr,0:ncorr,5))
+     allocate(tcfH_np(0:ncorr,0:ncorr,5)); allocate(tcfH_np_tot(0:ncorr,0:ncorr,5))
+     call read_eners(dH)
+  !     dw01avg = 0.0d0; dw12avg = 0.0d0; dw01sqavg = 0.0d0; dw12sqavg = 0.0d0
+  !     d2w01avg = 0.0d0; d2w01sqavg = 0.0d0; d2w12avg = 0.0d0; d2w12sqavg = 0.0d0
+  endif
+
+  write(6,*) ' Allocated arrays'
+
+  ! set the number of OpenMP threads
+  call omp_set_num_threads(20)
+
+  ! Loop over the waiting times
+  do iTw = 1, nTw
+
+     write(6,'(A,F12.2,A)') ' Waiting time = ',Tw(iTw)*fsperau,' fs'
+     call flush(6)
+
+     ! Zero TCFs etc.
+     tcf_rp_tot = dcmplx(0.0d0,0.0d0); tcf_np_tot = dcmplx(0.0d0,0.0d0) 
+     if(flag_fluc) then
+        tcfH_rp_tot = dcmplx(0.0d0,0.0d0); tcfH_np_tot = dcmplx(0.0d0,0.0d0)
+     endif
+     read_time = 0.0d0; tcf_time = 0.0d0
+     w01avg = 0.0d0; w01sqavg = 0.0d0; w12avg = 0.0d0; w12sqavg = 0.0d0     
+
+     ! Loop over the OH groups
+     call cpu_time(tstart)
+
+!$omp parallel do private(ioh,ta,tb,w01,w12,mu01,mu12,eOH,tcf_rp,tcf_np) &
+!$omp reduction(+:read_time,tcf_time,tcf_rp_tot,tcf_np_tot)
+     do ioh = 1, noh
+
+        ! Read in the field file for OH number ioh
+        call cpu_time(ta)
+        
+        call read_field(ioh, w01, w12, mu01, mu12, eOH)
+        write(6,*) ' Read field, ioh = ',ioh
+        call flush(6)
+        
+        call cpu_time(tb)
+        read_time = read_time + tb - ta
+        
+        ! Calculate the 2D-IR spectra TCF for the OH number ioh 
+        call cpu_time(ta)
+        
+        if(flag_fluc) then
+           call calc_tcf_fluc(ioh, iTw, w01, w12, mu01, mu12, eOH, dH, tcf_rp, tcf_np, tcfH_rp, tcfH_np)
+           
+           ! Add TCF to the total TCF
+           tcf_rp_tot = tcf_rp_tot + tcf_rp
+           tcf_np_tot = tcf_np_tot + tcf_np
+           tcfH_rp_tot = tcfH_rp_tot + tcfH_rp
+           tcfH_np_tot = tcfH_np_tot + tcfH_np
+        else
+           call calc_tcf(ioh, iTw, w01, w12, mu01, mu12, eOH, tcf_rp, tcf_np)
+           
+           ! Add TCF to the total TCF
+           tcf_rp_tot = tcf_rp_tot + tcf_rp
+           tcf_np_tot = tcf_np_tot + tcf_np
+        endif
+        
+        call cpu_time(tb)
+        tcf_time = tcf_time + tb - ta     
+        
+        write(6,'(A,I4,A,F10.2,A)') ' ioh = ',ioh,' time = ',tb-ta,' s'
+        call flush(6)
+        
+     enddo  ! end loop over OH groups
+!$omp end parallel do
+
+     write(6,*) ' Finished TCF calc'
+     call flush(6)
+
+     tcf_rp_tot = tcf_rp_tot/dcmplx(dble(noh),0d0); tcf_np_tot = tcf_np_tot/dcmplx(dble(noh),0d0)
+     w01avg = w01avg/dble(noh*ntimes); w01sqavg = w01sqavg/dble(noh*ntimes)
+     w12avg = w12avg/dble(noh*ntimes); w12sqavg = w12sqavg/dble(noh*ntimes)
+     dw01avg = dw01avg/dble(noh*ntimes); dw01sqavg = dw01sqavg/dble(noh*ntimes)
+     dw12avg = dw12avg/dble(noh*ntimes); dw12sqavg = dw12sqavg/dble(noh*ntimes)
+     d2w01avg = d2w01avg/dble(noh*ntimes); d2w01sqavg = d2w01sqavg/dble(noh*ntimes)
+     d2w12avg = d2w12avg/dble(noh*ntimes); d2w12sqavg = d2w12sqavg/dble(noh*ntimes)
+     if(flag_fluc) then
+        tcfH_rp_tot = tcfH_rp_tot/dcmplx(dble(noh),0d0)
+        tcfH_np_tot = tcfH_np_tot/dcmplx(dble(noh),0d0)
+     endif
+
+     ! Calculate (and output) the 2D-IR spectra from the total TCF
+     call cpu_time(ta)
+     if(flag_fluc) then
+        call spec_calc_fluc(iTw, tcf_rp_tot, tcf_np_tot, tcfH_rp_tot, tcfH_np_tot)
+     else
+        call spec_calc(iTw, tcf_rp_tot,tcf_np_tot)
+     endif
+     call cpu_time(tb)
+
+     call cpu_time(tend)
+     write(6,'(A,F12.2,A)') ' Waiting time = ',Tw(iTw)*fsperau,' fs'
+     write(6,'(A,F12.2,A,F10.2,A)') ' Read  cpu time = ',read_time,' s, ',read_time/60d0,' min'
+     write(6,'(A,F12.2,A,F10.2,A)') ' TCF   cpu time = ',tcf_time,' s, ',tcf_time/60d0,' min'
+     write(6,'(A,F12.2,A,F10.2,A)') ' FFT   cpu time = ',tb-ta,' s, ',(tb-ta)/60d0,' min'
+     write(6,'(A,F12.2,A,F10.2,A)') ' Total cpu time = ',tend-tstart,' s, ',(tend-tstart)/60d0,' min'
+
+  enddo  ! End loop over waiting times
+
+  deallocate(w01); deallocate(mu01); deallocate(w12); deallocate(mu12); deallocate(eOH)
+  deallocate(tcf_rp); deallocate(tcf_np); deallocate(tcf_rp_tot); deallocate(tcf_np_tot)
+
+  End Program spectra
+
+
---
+++ 2DIR_OLD/read_field.f90
@@ -0,0 +1,162 @@
+SUBROUTINE read_field(ioh, w01, w12, mu01, mu12, eOH)
+    USE time_data
+    USE map_data
+    USE freq_data
+    USE HDF5
+
+    IMPLICIT NONE
+
+    INTEGER, INTENT(IN) :: ioh
+    DOUBLE PRECISION, DIMENSION(ntimes), INTENT(OUT) :: w01, w12
+    DOUBLE PRECISION, DIMENSION(ntimes), INTENT(OUT) :: mu01, mu12
+    DOUBLE PRECISION, DIMENSION(ntimes,3), INTENT(OUT) :: eoh
+
+    INTEGER :: ioh, j, k
+    REAL, DIMENSION(2000) :: etmp
+    REAL, DIMENSION(ntimes, 3) :: eoh_tmp
+    DOUBLE PRECISION ::  muprime, x01tmp, x12tmp
+
+    ! HDF5 Variables
+    CHARACTER(len=8), PARAMETER :: filename = 'field.h5'
+    INTEGER(HID_T) :: file_id, dataset_id
+    INTEGER :: ERROR_FLAG
+
+    INTEGER(HSIZE_T), DIMENSION(1) :: dot_dims 
+    INTEGER(HSIZE_T), DIMENSION(2) :: eoh_dims 
+
+
+    CHARACTER(len=20) :: dataset_name
+
+!I. Read the field file *******************************************************
+    ! Open the hdf5 library
+    CALL h5open_f(ERROR_FLAG)
+    ! Open the hdf5 file
+    CALL h5fopen_f(filename, H5F_ACC_RDONLY_F, file_id, ERROR_FLAG)
+
+    ! Read the value of the dot product ***
+    ! Set dataset name
+    WRITE(dataset_name, '(A, I0)') 'dot_', ioh
+    ! Open the existing dataset
+    CALL h5dopen_f(file_id, dataset_name, dataset_id, ERROR_FLAG)
+    ! Read the dataset
+    CALL h5dread_f(dataset_id, H5T_NATIVE_REAL, etmp, dot_dims, ERROR_FLAG)
+    ! Close the dataset
+    CALL h5dclose_f(dataset_id, ERROR_FLAG)
+
+    ! Read the value of the unit vector eoh_tmp ***
+    ! Set dataset name
+    WRITE(dataset_name, '(A, I0)') 'dot_', ioh
+    ! Open the existing dataset
+    CALL h5dopen_f(file_id, dataset_name, dataset_id, ERROR_FLAG)
+    ! Read the dataset
+    CALL h5dread_f(dataset_id, H5T_NATIVE_REAL, eoh_tmp, eoh_dims, ERROR_FLAG)
+    ! Close the dataset
+    CALL h5dclose_f(dataset_id, ERROR_FLAG)
+
+    ! Close the file
+    CALL h5fclose_f(file_id, ERROR_FLAG)
+    ! Close the library
+    CALL h5close_f(ERROR_FLAG)
+
+! II. Calculate the field parameters ******************************************
+    DO k=1, ntimes
+        w01(k) = c0 + c1*etmp + c2*etmp**2
+        w01avg = w01avg + w01(k)
+        w01sqavg = w01sqavg + w01(k)**2
+
+        w12(k) = c3 + c4*etmp + c5*etmp**2     
+        w12avg = w12avg + w12(k)
+        w12sqavg = w12sqavg + w12(k)**2
+
+        ! convert unit vector to transition dipole moment
+        muprime = b0 + b1*etmp + b2*etmp**2
+
+        x01tmp = d0 + d1*w01(k)     
+        mu01(k) = muprime*x01tmp
+
+        x12tmp = d2 + d3*w12(k)     
+        mu12(k) = muprime*x12tmp
+
+        eoh(k,:) = eoh_tmp(k,:)
+    END DO
+
+
+END SUBROUTINE read_field
+
+
+!   Subroutine to read in the field parameters from field.xxx
+!     and calculate the derivatives of the averages
+
+  Subroutine read_field_fluc(ioh, w01, w12, mu01, mu12, eOH, dH)
+
+  use time_data
+  use map_data
+  use freq_data
+  use fluc_data
+
+  implicit none
+  integer :: ioh, j, k, n
+  character*4 :: ext
+  double precision :: etmp, muprime, x01tmp, x12tmp
+  double precision, dimension(ntimes) :: w01, w12
+  double precision, dimension(ntimes) :: mu01, mu12
+  double precision, dimension(ntimes,3) :: eOH
+  double precision, dimension(ntimes,4) :: dH
+
+
+
+  ! Open the field field file for OH group iOH
+  if(ndigits.eq.4) then
+     write(ext,'(I0.4)') ioh 
+  elseif(ndigits.eq.3) then
+     write(ext,'(I0.3)') ioh
+  else
+     write(6,*) ' Invalid ndigits, only 3 or 4 are allowed'
+     stop
+  endif
+
+  ! Open the field file
+  open(ioh+100,file='field_files/field.'//ext//'',status='old')
+
+  do k = 1 , ntimes
+
+     ! read in data from file (here mu = e_OH)
+     read(ioh+100,*) etmp, (eOH(k,j), j=1,3)
+     
+     ! convert etmp to frequency
+     w01(k) = c0 + c1*etmp + c2*etmp**2     
+     w01avg = w01avg + w01(k)
+     w01sqavg = w01sqavg + w01(k)**2
+
+     w12(k) = c3 + c4*etmp + c5*etmp**2     
+     w12avg = w12avg + w12(k)
+     w12sqavg = w12sqavg + w12(k)**2
+
+
+     ! calculate the fluctuation weighted averages
+     do n = 1, 5
+        dw01avg(n) = dw01avg(n) + dH(k,n)*w01(k)
+        dw01sqavg(n) = dw01sqavg(n) + dH(k,n)*w01(k)**2
+        d2w01avg(n) = d2w01avg(n) + (dH(k,n)**2 - dH2avg(n))*w01(k)
+        d2w01sqavg(n) = d2w01sqavg(n) + (dH(k,n)**2 - dH2avg(n))*w01(k)**2
+
+        dw12avg(n) = dw12avg(n) + dH(k,n)*w12(k)
+        dw12sqavg(n) = dw12sqavg(n) + dH(k,n)*w12(k)**2
+        d2w12avg(n) = d2w12avg(n) + (dH(k,n)**2 - dH2avg(n))*w12(k)
+        d2w12sqavg(n) = d2w12sqavg(n) + (dH(k,n)**2 - dH2avg(n))*w12(k)**2
+     enddo
+
+     ! convert unit vector to transition dipole moment
+     muprime = b0 + b1*etmp + b2*etmp**2
+
+     x01tmp = d0 + d1*w01(k)     
+     mu01(k) = muprime*x01tmp
+
+     x12tmp = d2 + d3*w12(k)
+     mu12(k) = muprime*x12tmp
+
+  enddo
+  rewind(ioh+100)
+  close(ioh+100)
+
+  End subroutine read_field_fluc
