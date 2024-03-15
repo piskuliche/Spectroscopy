413dc2583d7f09ea28af7b1205d32dcd7252c501
Zeke Piskulich <piskulichz@gmail.com>
2024-03-15 14:18:59 -0400

    updated spectral averaging

--- Python/average_spectra.py
+++ Python/average_spectra.py
@@ -11,25 +11,65 @@ import numpy as np
 import os
 
 
+def Average_Spectra(basename, start_idx, stop_idx, outfile='ir_spectra.dat'):
+    """ This averages the spectra across directories, and then saves it to a file.
+
+
+    Parameters:
+    -----------
+    basename : str
+        The basename of the file containing the spectra.
+    start_idx : int
+        The first directory to read.
+    stop_idx : int
+        The last directory to read.
+    outfile : str
+        The name of the file to save the averaged spectra to.
+
+    Returns:
+    --------
+    spectra : tuple
+        A tuple containing the frequencies, real and imaginary parts of the spectra.
+
+    """
+    wfreq = np.genfromtxt(
+        f"run_dir/{start_idx}/{basename}", usecols=0, unpack=True)
+    spectral = []
+    for i in range(start_idx, stop_idx):
+        temporary = np.genfromtxt(
+            f"run_dir/{i}/{basename}", usecols=1, unpack=True)
+        spectral.append(temporary)
+    averaged = np.average(spectral, axis=0)
+    np.savetxt(outfile, np.c_[wfreq, averaged])
+    return
+
+
 def Average_SFG(basename,  start_idx, stop_idx, outfile='sfg_spectra.dat'):
     """
     This function averages the SFG spectra across directories, and then saves it to a file.
 
-    Args:
-        basename (str): The basename of the file containing the spectra.
-        start_idx (int): The first directory to read.
-        stop_idx (int): The last directory to read.
-        outfile (str): The name of the file to save the averaged spectra to.
+    Parameters:
+    -----------
+    basename : str
+        The basename of the file containing the spectra.
+    start_idx : int
+        The first directory to read.
+    stop_idx : int
+        The last directory to read.
+    outfile : str
+        The name of the file to save the averaged spectra to.
 
     Returns:
-        spectra (tuple): A tuple containing the frequencies, real and imaginary parts of the spectra.
+    --------
+    spectra : tuple
+        A tuple containing the frequencies, real and imaginary parts of the spectra.
 
-    Outputs:
-        A file containing the averaged spectra.
+
     """
 
     # Read in the first file to get the frequency array
-    wfreq = np.genfromtxt("run_dir/0/%s" % basename, usecols=0, unpack=True)
+    wfreq = np.genfromtxt(
+        f"run_dir/{start_idx}/{basename}", usecols=0, unpack=True)
     real_part = []
     imag_part = []
     for i in range(start_idx, stop_idx):
@@ -63,6 +103,17 @@ if __name__ == "__main__":
     start_idx = args.start
     stop_idx = args.stop
     spec_type = args.spectra
-
-    if (spec_type == 2):
+    if spec_type == 0:
+        for name in ["ir_spectrum.dat", "freq_dist.dat", "spec_dens.dat"]:
+            Average_Spectra(name, start_idx, stop_idx,
+                            outfile=f"averaged_{name}")
+    elif spec_type == 1:
+        for name in ["iso_spectrum.dat", "depol_spectrum.dat", "vv_spectrum.dat", "vh_spectrum.dat"]:
+            Average_Spectra(name, start_idx, stop_idx,
+                            outfile=f"averaged_{name}")
+    elif spec_type == 2:
         Average_SFG(basename, start_idx, stop_idx)
+    elif spec_type == 3:
+        raise NotImplementedError("2D-IR not yet implemented")
+    else:
+        raise ValueError("The valid options are [0-3]")
