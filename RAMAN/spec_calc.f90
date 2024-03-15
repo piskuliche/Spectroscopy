3dd6f6f7e0e681a78f2f1d202a4c434bc82dc763
Zeke Piskulich <piskulichz@gmail.com>
2024-03-15 09:40:45 -0400

    Refactor Spec_Calc subroutine in RAMAN/spec_calc.f90 to include additional output statements

--- RAMAN/spec_calc.f90
+++ RAMAN/spec_calc.f90
@@ -35,7 +35,8 @@ SUBROUTINE Spec_Calc(vv_tcf_tot, vh_tcf_tot)
         vv_tcf_tot(i) = vv_tcf_tot(i)*DCMPLX(DEXP(-0.5d0*ti/T1),0d0)
         vh_tcf_tot(i) = vh_tcf_tot(i)*DCMPLX(DEXP(-0.5d0*ti/T1),0d0)
     END DO
-
+    WRITE(6,*) ' Vibrational Relaxation Factor = ',DEXP(-0.5d0*dt/T1)
+    WRITE(6,*) '<w> = ',w01_avg
     ! ...B. The average frequency exp{-i*<w>*t}
     DO i = 0, ncorr
         ti = float(i)*dt
