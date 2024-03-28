2c4e01f7abf546c917e785958c23bab2189180d6
Zeke Piskulich <piskulichz@gmail.com>
2024-03-28 12:03:06 -0400

    Update README.md with installation and usage instructions

--- README.md
+++ README.md
@@ -1,5 +1,22 @@
 # Readme
 
+## Unfinished Business
+
+The 2DIR part of the code doesn't take advantage of the significant overlap between the other types of spectroscopic codes with the original. Currently, you need to define an empirical map for 
+
 ## Installation
 
-This package is a package of multiple different submodules, each a different type of spectroscopy. At some point, I would like to interface all of them together into a "grand" package, but for now each module must be compiled, and run, separately. 
+This package can be installed with cmake. The general approach for such a build is the following.
+
+mkdir build
+
+cd build
+
+cmake ..
+
+make
+
+
+## Usage 
+
+
--- src/2DIR/spec_calc.f90
+++ src/2DIR/spec_calc.f90
@@ -1,271 +1,98 @@
 !   Subroutine to Fourier Transform the total TCF to calculate the IR spectrum
 
-   Subroutine spec_calc(iTw, tcf_rp_tot, tcf_np_tot)
-  
-      use time_data
-      use map_data
-      use freq_data
-      use constants
-      
-      implicit none
-      include '../Shared/fftw3.f'
-      integer :: i, j, ineg, jneg, k, k_rp, p, nt1, nt3, iTw
-      integer*8 :: plan_rp, plan_np
-      double precision :: w_spec1, w_spec3, ti, pi, fact1, fact3
-      character*5 :: cTw
-      
-      double complex, dimension(0:ncorr,0:ncorr) :: tcf_rp_tot, tcf_np_tot
-      double complex, allocatable, dimension(:,:) :: input_rp, input_np, spec_rp, spec_np
-      
+SUBROUTINE spec_calc(iTw, tcf_rp_tot, tcf_np_tot)
 
-      pi = 4.0d0*datan(1.0d0)
-      
-      ! Write out the average frequencies
-      write(6,*) ' <w01>   = ',w01_avg*cmiperau
-      write(6,*) ' <w12>   = ',w12_avg*cmiperau
-      write(6,*) ' <w01^2> = ',w01_sq_avg*cmiperau**2
-      write(6,*) ' <w12^2> = ',w12_sq_avg*cmiperau**2
-      
-      ! Initialize parameters for Fourier Transform
-      
-      ! Calculate the time domain grid needed to get the desired frequency resolution
-      p = int(dlog(2.0d0*pi/(dt*w_resol))/dlog(2.0d0))
-      nt1 = 2**p
-      nt3 = 2**p
-      write(6,*) ' Time Grid power, 2^ = ',p
-      write(6,*) ' Time Grid Size t1   = ',nt1
-      write(6,*) ' Time Grid Size t3   = ',nt3
-      
-      allocate(input_rp(nt1,nt3)); allocate(spec_rp(nt1,nt3))
-      allocate(input_np(nt1,nt3)); allocate(spec_np(nt1,nt3))
-      
-      call dfftw_plan_dft_2d(plan_rp, nt1, nt3, input_rp, spec_rp, FFTW_BACKWARD, FFTW_ESTIMATE)
-      call dfftw_plan_dft_2d(plan_np, nt1, nt3, input_np, spec_np, FFTW_BACKWARD, FFTW_ESTIMATE)
-      
-      ! Write waiting time to a character string
-      write(cTw,'(I0.5)') nint(Tw(iTw)*fsperau)
-      
-      ! Pad the calculated TCF with zeroes to get the desired freq resolution before FTing
-      input_rp = dcmplx(0.0d0,0.0d0); input_np = dcmplx(0.0d0,0.0d0)
-      do i = 0, min(nt1/2 - 1,ncorr - 1)
-         do j = 0, min(nt3/2 - 1,ncorr - 1)
-            ! Fill the first quadrant (t1, t3 > 0) as given
-            input_rp(i+1,j+1) = tcf_rp_tot(i,j)
-            input_np(i+1,j+1) = tcf_np_tot(i,j)
-            
-            ! Symmetrize the other three quadrants
-            ineg = nt1 - i
-            jneg = nt3 - j
-            input_rp(i+1, jneg) = tcf_rp_tot(i,j) 
-            input_np(i+1, jneg) = tcf_np_tot(i,j) 
-            input_rp(ineg, j+1) = tcf_rp_tot(i,j) 
-            input_np(ineg, j+1) = tcf_np_tot(i,j) 
-            input_rp(ineg, jneg) = tcf_rp_tot(i,j) 
-            input_np(ineg, jneg) = tcf_np_tot(i,j) 
-         enddo
-      enddo
-      
-      call dfftw_execute(plan_rp)
-      call dfftw_execute(plan_np)
+   USE time_data
+   USE map_data
+   USE freq_data
+   USE constants
+   
+   IMPLICIT NONE
+   INCLUDE '../Shared/fftw3.f'
+   INTEGER :: i, j, ineg, jneg, k, k_rp, p, nt1, nt3, iTw
+   INTEGER*8 :: plan_rp, plan_np
+   DOUBLE PRECISION :: w_spec1, w_spec3, ti, pi, fact1, fact3
+   CHARACTER*5 :: cTw
+   
+   DOUBLE COMPLEX, DIMENSION(0:ncorr,0:ncorr) :: tcf_rp_tot, tcf_np_tot
+   DOUBLE COMPLEX, ALLOCATABLE, DIMENSION(:,:) :: input_rp, input_np, spec_rp, spec_np
+   
+
+   pi = 4.0d0*datan(1.0d0)
+   
+   ! Write out the average frequencies
+   WRITE(6,*) ' <w01>   = ',w01_avg*cmiperau
+   WRITE(6,*) ' <w12>   = ',w12_avg*cmiperau
+   WRITE(6,*) ' <w01^2> = ',w01_sq_avg*cmiperau**2
+   WRITE(6,*) ' <w12^2> = ',w12_sq_avg*cmiperau**2
+   
+   ! Initialize parameters for Fourier Transform
+   
+   ! Calculate the time domain grid needed to get the desired frequency resolution
+   p = int(dlog(2.0d0*pi/(dt*w_resol))/dlog(2.0d0))
+   nt1 = 2**p
+   nt3 = 2**p
+   WRITE(6,*) ' Time Grid power, 2^ = ',p
+   WRITE(6,*) ' Time Grid Size t1   = ',nt1
+   WRITE(6,*) ' Time Grid Size t3   = ',nt3
+   
+   ALLOCATE(input_rp(nt1,nt3)); ALLOCATE(spec_rp(nt1,nt3))
+   ALLOCATE(input_np(nt1,nt3)); ALLOCATE(spec_np(nt1,nt3))
+   
+   CALL dfftw_plan_dft_2d(plan_rp, nt1, nt3, input_rp, spec_rp, FFTW_BACKWARD, FFTW_ESTIMATE)
+   CALL dfftw_plan_dft_2d(plan_np, nt1, nt3, input_np, spec_np, FFTW_BACKWARD, FFTW_ESTIMATE)
+   
+   ! Write waiting time to a CHARACTER string
+   WRITE(cTw,'(I0.5)') NINT(Tw(iTw)*fsperau)
+   
+   ! Pad the calculated TCF with zeroes to get the desired freq resolution before FTing
+   input_rp = dcmplx(0.0d0,0.0d0); input_np = dcmplx(0.0d0,0.0d0)
+   DO i = 0, MIN(nt1/2 - 1,ncorr - 1)
+      DO j = 0, MIN(nt3/2 - 1,ncorr - 1)
+         ! Fill the first quadrant (t1, t3 > 0) as given
+         input_rp(i+1,j+1) = tcf_rp_tot(i,j)
+         input_np(i+1,j+1) = tcf_np_tot(i,j)
          
-      ! Write out the calculated spectrum
-      open(22,file='2dir_imag_'//trim(cTw)//'.dat')
-      open(23,file='2dir_real_'//trim(cTw)//'.dat')
-      !         open(24,file='2dtcf_imag_'//trim(cTw)//'.dat')
-      !         open(25,file='2dtcf_real_'//trim(cTw)//'.dat')
-
-         ! Write out the TCF
-
-!         do k = 0, ncorr
-!            do j = 0, ncorr
-!               write(24,'(3F16.8)') dble(k)*dt*fsperau, dble(j)*dt*fsperau, dimag(tcf_tot(k,j,iTw))
-!               write(25,'(3F16.8)') dble(k)*dt*fsperau, dble(j)*dt*fsperau, dreal(tcf_tot(k,j,iTw))
-!            enddo
-!         enddo
-         
+         ! Symmetrize the other three quadrants
+         ineg = nt1 - i
+         jneg = nt3 - j
+         input_rp(i+1, jneg) = tcf_rp_tot(i,j) 
+         input_np(i+1, jneg) = tcf_np_tot(i,j) 
+         input_rp(ineg, j+1) = tcf_rp_tot(i,j) 
+         input_np(ineg, j+1) = tcf_np_tot(i,j) 
+         input_rp(ineg, jneg) = tcf_rp_tot(i,j) 
+         input_np(ineg, jneg) = tcf_np_tot(i,j) 
+      ENDDO
+   ENDDO
+   
+   CALL dfftw_execute(plan_rp)
+   CALL dfftw_execute(plan_np)
+      
+   ! Write out the calculated spectrum
+   OPEN(22,file='2dir_imag_'//trim(cTw)//'.dat')
+   OPEN(23,file='2dir_real_'//trim(cTw)//'.dat')
+      
 !!! N/A    ! When the average frequency is subtracted from the phase the spectrum must be split in the middle
 !!! N/A    ! First print out the frequencies below the average
-      fact1 = (2d0*pi/dt)/dble(nt1); fact3 = (2d0*pi/dt)/dble(nt3)
-      do k = 1, nt1/2 
-         w_spec1 = fact1*dble(k-1)
-         k_rp = modulo(nt1 + 1 - k, nt1 + 1) + floor(dble(nt1 + 1 - k)/dble(nt1+1))
-         do j = 1, nt3/2 
-            w_spec3 = fact3*dble(j-1)
-            if(w_spec1.ge.w1min.and.w_spec1.le.w1max.and.w_spec3.ge.w3min.and.w_spec3.le.w3max) then
-               write(22,'(2F16.6,2F18.6)') w_spec1*cmiperau, w_spec3*cmiperau, dimag(spec_rp(k_rp,j) + spec_np(k,j))
-               write(23,'(2F16.6,2F18.6)') w_spec1*cmiperau, w_spec3*cmiperau, dreal(spec_rp(k_rp,j) + spec_np(k,j))
-            endif
-            
-         enddo
-      enddo
-      
-      close(22); close(23)
-      
-      call dfftw_destroy_plan(plan_rp)
-      call dfftw_destroy_plan(plan_np)
-      deallocate(input_rp,input_np)
-      
-    End Subroutine spec_calc
-
-
-
-!   Subroutine to Fourier Transform the total TCF to calculate the IR spectrum
-!     and the energy fluctuated versions
-
-   Subroutine spec_calc_fluc(iTw, tcf_rp_tot, tcf_np_tot, tcfH_rp_tot, tcfH_np_tot)
-  
-      use time_data
-      use map_data
-      use freq_data
-      use constants
-      
-      implicit none
-      include '../Shared/fftw3.f'
-      integer :: i, j, ineg, jneg, k, k_rp, p, nt1, nt3, iTw, iH
-      integer*8 :: plan_rp, plan_np
-      double precision :: w_spec1, w_spec3, ti, pi, fact1, fact3
-      character*5 :: cTw
-      character*2, dimension(5) :: Hlab
-      
-      double complex, dimension(0:ncorr,0:ncorr) :: tcf_rp_tot, tcf_np_tot
-      double complex, dimension(0:ncorr,0:ncorr,5) :: tcfH_rp_tot, tcfH_np_tot
-      double complex, allocatable, dimension(:,:) :: input_rp, input_np, spec_rp, spec_np
-      
-
-      pi = 4.0d0*datan(1.0d0)
-      Hlab(1) = 'H'; Hlab(2) = 'KE'; Hlab(3) = 'LJ'; Hlab(4) = 'Co'; Hlab(5) = 'V'
-      
-      ! Write out the average frequencies
-      write(6,*) ' <w01>   = ',w01_avg*cmiperau
-      write(6,*) ' <w12>   = ',w12_avg*cmiperau
-      write(6,*) ' <w01^2> = ',w01_sq_avg*cmiperau**2
-      write(6,*) ' <w12^2> = ',w12_sq_avg*cmiperau**2
-      
-      ! Initialize parameters for Fourier Transform
-      
-      ! Calculate the time domain grid needed to get the desired frequency resolution
-      p = int(dlog(2.0d0*pi/(dt*w_resol))/dlog(2.0d0))
-      nt1 = 2**p
-      nt3 = 2**p
-      write(6,*) ' Time Grid power, 2^ = ',p
-      write(6,*) ' Time Grid Size t1   = ',nt1
-      write(6,*) ' Time Grid Size t3   = ',nt3
-      
-      allocate(input_rp(nt1,nt3)); allocate(spec_rp(nt1,nt3))
-      allocate(input_np(nt1,nt3)); allocate(spec_np(nt1,nt3))
-
-      
-      call dfftw_plan_dft_2d(plan_rp, nt1, nt3, input_rp, spec_rp, FFTW_BACKWARD, FFTW_ESTIMATE)
-      call dfftw_plan_dft_2d(plan_np, nt1, nt3, input_np, spec_np, FFTW_BACKWARD, FFTW_ESTIMATE)
-      
-      ! Write the waiting time to a character string  
-      write(cTw,'(I0.5)') nint(Tw(iTw)*fsperau)
-         
-      ! Pad the calculated TCF with zeroes to get the desired freq resolution before FTing
-      input_rp = dcmplx(0.0d0,0.0d0); input_np = dcmplx(0.0d0,0.0d0)
-      do i = 0, min(nt1/2 - 1,ncorr - 1)
-         do j = 0, min(nt3/2 - 1,ncorr - 1)
-            ! Fill the first quadrant (t1, t3 > 0) as given
-            input_rp(i+1,j+1) = tcf_rp_tot(i,j)
-            input_np(i+1,j+1) = tcf_np_tot(i,j)
-            
-            ! Symmetrize the other three quadrants
-            ineg = nt1 - i
-            jneg = nt3 - j
-            input_rp(i+1, jneg) = tcf_rp_tot(i,j) 
-            input_np(i+1, jneg) = tcf_np_tot(i,j) 
-            input_rp(ineg, j+1) = tcf_rp_tot(i,j) 
-            input_np(ineg, j+1) = tcf_np_tot(i,j) 
-            input_rp(ineg, jneg) = tcf_rp_tot(i,j) 
-            input_np(ineg, jneg) = tcf_np_tot(i,j) 
-         enddo
-      enddo
-      
-      call dfftw_execute(plan_rp)
-      call dfftw_execute(plan_np)
-         
-      ! Write out the calculated spectrum
-      open(23,file='2dir_real_'//trim(cTw)//'.dat')
-      open(24,file='2dir_imag_'//trim(cTw)//'.dat')
-
-      !         open(24,file='2dtcf_imag_'//trim(cTw)//'.dat')
-      !         open(25,file='2dtcf_real_'//trim(cTw)//'.dat')
-
-         ! Write out the TCF
-
-!         do k = 0, ncorr
-!            do j = 0, ncorr
-!               write(24,'(3F16.8)') dble(k)*dt*fsperau, dble(j)*dt*fsperau, dimag(tcf_tot(k,j,iTw))
-!               write(25,'(3F16.8)') dble(k)*dt*fsperau, dble(j)*dt*fsperau, dreal(tcf_tot(k,j,iTw))
-!            enddo
-!         enddo
-         
-      fact1 = (2d0*pi/dt)/dble(nt1); fact3 = (2d0*pi/dt)/dble(nt3)
-      do k = 1, nt1/2 
-         w_spec1 = fact1*dble(k-1)
-         k_rp = modulo(nt1 + 1 - k, nt1 + 1) + floor(dble(nt1 + 1 - k)/dble(nt1+1))
-         do j = 1, nt3/2 
-            w_spec3 = fact3*dble(j-1)
-            if(w_spec1.ge.w1min.and.w_spec1.le.w1max.and.w_spec3.ge.w3min.and.w_spec3.le.w3max) then
-               write(23,'(4F16.8)') w_spec1*cmiperau, w_spec3*cmiperau, dreal(spec_rp(k_rp,j) + spec_np(k,j))
-               write(24,'(4F16.8)') w_spec1*cmiperau, w_spec3*cmiperau, dimag(spec_rp(k_rp,j) + spec_np(k,j))
-            endif
-            
-         enddo
-      enddo
-      
-      close(23); close(24) 
-
-      do iH = 1, 4
-         ! Pad the calculated TCF with zeroes to get the desired freq resolution before FTing
-         input_rp = dcmplx(0.0d0,0.0d0); input_np = dcmplx(0.0d0,0.0d0)
-         do i = 0, min(nt1/2 - 1,ncorr - 1)
-            do j = 0, min(nt3/2 - 1,ncorr - 1)
-               ! Fill the first quadrant (t1, t3 > 0) as given
-               input_rp(i+1,j+1) = tcfH_rp_tot(i,j,iH)
-               input_np(i+1,j+1) = tcfH_np_tot(i,j,iH)
-                  
-               ! Symmetrize the other three quadrants
-               ineg = nt1 - i
-               jneg = nt3 - j
-               input_rp(i+1, jneg) = tcfH_rp_tot(i,j,iH) 
-               input_np(i+1, jneg) = tcfH_np_tot(i,j,iH) 
-               input_rp(ineg, j+1) = tcfH_rp_tot(i,j,iH) 
-               input_np(ineg, j+1) = tcfH_np_tot(i,j,iH) 
-               input_rp(ineg, jneg) = tcfH_rp_tot(i,j,iH) 
-               input_np(ineg, jneg) = tcfH_np_tot(i,j,iH) 
-            enddo
-         enddo
-            
-         call dfftw_execute(plan_rp)
-         call dfftw_execute(plan_np)
-         
-         ! Write out the calculated spectrum
-         open(23,file='2dir'//trim(Hlab(iH))//'_real_'//trim(cTw)//'.dat')
-         open(24,file='2dir'//trim(Hlab(iH))//'_imag_'//trim(cTw)//'.dat')
-
-         fact1 = (2d0*pi/dt)/dble(nt1); fact3 = (2d0*pi/dt)/dble(nt3)
-         do k = 1, nt1/2 
-            w_spec1 = fact1*dble(k-1)
-            k_rp = modulo(nt1 + 1 - k, nt1 + 1) + floor(dble(nt1 + 1 - k)/dble(nt1+1))
-            do j = 1, nt3/2 
-               w_spec3 = fact3*dble(j-1)
-               if(w_spec1.ge.w1min.and.w_spec1.le.w1max.and.w_spec3.ge.w3min.and.w_spec3.le.w3max) then
-                  write(23,'(4F16.8)') w_spec1*cmiperau, w_spec3*cmiperau, dreal(spec_rp(k_rp,j) + spec_np(k,j))
-                  write(24,'(4F16.8)') w_spec1*cmiperau, w_spec3*cmiperau, dimag(spec_rp(k_rp,j) + spec_np(k,j))
-               endif
-            enddo
-         enddo
-         
-      enddo  ! Loop over energy fluctuation components
-         
-      close(23); close(24) 
-      
-      call dfftw_destroy_plan(plan_rp)
-      call dfftw_destroy_plan(plan_np)
-      deallocate(input_rp,input_np)
-      
-    End Subroutine spec_calc_fluc
-
-
+   fact1 = (2d0*pi/dt)/DBLE(nt1); fact3 = (2d0*pi/dt)/DBLE(nt3)
+   DO k = 1, nt1/2 
+      w_spec1 = fact1*DBLE(k-1)
+      k_rp = MODULO(nt1 + 1 - k, nt1 + 1) + FLOOR(DBLE(nt1 + 1 - k)/DBLE(nt1+1))
+      DO j = 1, nt3/2 
+         w_spec3 = fact3*DBLE(j-1)
+         IF(w_spec1.ge.w1min.and.w_spec1.le.w1max.and.w_spec3.ge.w3min.and.w_spec3.le.w3max) THEN
+            WRITE(22,'(2F16.6,2F18.6)') w_spec1*cmiperau, w_spec3*cmiperau, dimag(spec_rp(k_rp,j) + spec_np(k,j))
+            WRITE(23,'(2F16.6,2F18.6)') w_spec1*cmiperau, w_spec3*cmiperau, dreal(spec_rp(k_rp,j) + spec_np(k,j))
+         ENDIF
+      ENDDO
+   ENDDO
+   
+   close(22); close(23)
+   
+   CALL dfftw_destroy_plan(plan_rp)
+   CALL dfftw_destroy_plan(plan_np)
+   
+   DEALLOCATE(input_rp); DEALLOCATE(input_np)
+   
+End Subroutine spec_calc
 
