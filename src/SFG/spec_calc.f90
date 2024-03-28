ba088a3cf7bc416559e2934b17927954d8fa3c89
Zeke Piskulich <piskulichz@gmail.com>
2024-03-28 15:09:11 -0400

    Fix variable name typo in spec_calc.f90

--- src/SFG/spec_calc.f90
+++ src/SFG/spec_calc.f90
@@ -66,7 +66,7 @@ SUBROUTINE Spec_Calc(tcf_tot)
     ! IV. Write out the spectrum
     DO i=1, nt
         w_spec = (2d0*pi/dt)*dfloat(i-1)/dfloat(nt)
-        IF (w_spec .ge. wmin .and. w_spec .le. wmax) THEN
+        IF (w_spec .ge. w1min .and. w_spec .le. w1max) THEN
             WRITE(22,*) w_spec*cmiperau, DREAL(sfg_spec(i)), -DIMAG(sfg_spec(i))
         ENDIF
     ENDDO
