0af1d5b3a5d59879b5d8409ff1f3a9e6324c1939
Zeke Piskulich <piskulichz@gmail.com>
2024-04-23 21:27:39 -0400

    Refactor FFCF_CALC subroutine to use FFCF_TCF_CALC with ffcf parameter

--- src/FFCF/ffcf_calc.f90
+++ src/FFCF/ffcf_calc.f90
@@ -60,8 +60,9 @@ PROGRAM FFCF_CALC
         DO ioh=(chunk-1)*nperchunk+1, MIN(chunk*nperchunk, noh)
             WRITE(*,*) "ioh: ", ioh
             iper = ioh - (chunk-1)*nperchunk
-            CALL FFCF_TCF_CALC(w01(iper,:), ffcf)
-            ffcf_tot = ffcf_tot + ffcf
+            WRITE(*,*) "iper: ", iper
+            CALL FFCF_TCF_CALC(w01(iper,:), ffcf(:))
+            ffcf_tot = ffcf_tot + ffcf(:)
         ENDDO
     ENDDO
 
